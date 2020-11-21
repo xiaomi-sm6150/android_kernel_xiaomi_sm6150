e6c7b8e30d83da18b361990fba1ebfaac5818dcf
Yaroslav Furman <yaro330@gmail.com>
2020-11-21 22:39:54 +0300

    input: touchscreen: nt36672c: Remove MTK parts
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Change-Id: I77395bade0b8bef54f623a65ee003b4278000e1c
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.h
+++ drivers/input/touchscreen/nt36672c/nt36xxx.h
@@ -34,21 +34,12 @@
 
 #include "nt36xxx_mem_map.h"
 
-#ifdef CONFIG_MTK_SPI
-#include "mtk_spi.h"
-#endif
-
-#ifdef CONFIG_SPI_MT65XX
-#include <linux/platform_data/spi-mt65xx.h>
-#endif
-
 /*Lock down info size*/
 #define NVT_LOCKDOWN_SIZE			8
 
 #define PINCTRL_STATE_ACTIVE		"pmx_ts_active"
 #define PINCTRL_STATE_SUSPEND		"pmx_ts_suspend"
 #define PINCTRL_STATE_RELEASE		"pmx_ts_release"
-
 
 #define NVT_DEBUG 1
 
@@ -159,12 +150,6 @@ struct nvt_ts_data {
 	uint8_t *xbuf;
 	struct mutex xbuf_lock;
 	bool irq_enabled;
-#ifdef CONFIG_MTK_SPI
-	struct mt_chip_conf spi_ctrl;
-#endif
-#ifdef CONFIG_SPI_MT65XX
-    struct mtk_chip_config spi_ctrl;
-#endif
 	struct pinctrl *ts_pinctrl;
 	struct pinctrl_state *pinctrl_state_active;
 	struct pinctrl_state *pinctrl_state_suspend;
